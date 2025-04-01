cf1a252eb9c246ffaddf514124bfb363b53c53a7
Pablo revuelta <revueltaapablo@gmail.com>
2025-04-01 17:06:03 +0200

    Dividing the lists and dics function and changing exporting to csv

---
+++ ihrat/src/list_dics_functions.py
@@ -0,0 +1,66 @@
+import geopandas as gpd
+import csv
+
+def shp_to_dict(file):
+    #Export the .shp file into a geo data frame and then into a dictionary
+    geodataframe = gpd.read_file(file)
+    dict = geodataframe.set_index('ID_PARCELA').T.to_dict('dict')
+    return dict
+def add_listofdics_to_dicofdics(dic,listofdics,newkeys):
+    #Add to each entry of a dictionary of dictionaries the values stored in a list of dictionaries.
+    #To each entry of the dictionary, add the corresponding dictionary from the list
+    for i, j in zip(dic, listofdics):
+        dic[i].update(j)
+    keyslist = list(set(key for diccionario in listofdics for key in diccionario.keys()))
+    for i in range(len(keyslist)):
+        change_keys_dic(dic, keyslist[i], newkeys[i])
+def add_list_to_dicofdics(dic, list, subkey):
+    # Add to each entry of a dictionary of dictionaries the values stored in a list.
+    # To each entry of the dictionary, add the corresponding value from the list
+    for (keydic, dicdic), value in zip(dic.items(), list):
+        dicdic[subkey] = value
+def add_dic_to_dicofdics(dicofdics,dic,key):
+    # Add to each entry of a dictionary of dictionaries the values stored in a dictionary, with the same key.
+    # To each entry of the dictionary od dictionaries, add the corresponding value from the dictionary
+    for clave in dicofdics:
+            dicofdics[clave][key] = dic[clave]
+def product_columns_dic(dic,key1,key2):
+    # Multiply two columns of a dic. Return a dictionary with the same keys and the products
+    product_dic = {}
+    for key, subdic in dic.items():
+        product = subdic[key1] * subdic[key2]
+        product_dic[key] = product
+    return product_dic
+def dictoddics_to_csv(dic,path):
+    #Export the content of dictionary to a .csv file. Each entry is in a single row, they keys are in the first and
+    #the keys of the sub dictionaries are the headers. (ORDENAR FALTA)
+    with open(path, mode='w', newline='') as file:
+        # Get all unique fieldnames from the dictionaries and write them as first row
+        fieldnames = set()
+        for sub_dict in dic.values():
+            fieldnames.update(sub_dict.keys())
+        fieldnames = list(fieldnames)
+        fieldnames.insert(0, 'Building ID')  # Include the 'Key' column if needed
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        # Write the header row (keys of the inner dictionaries)
+        writer.writeheader()
+        # Write each row (each inner dictionary as a row)
+        for key, sub_dict in dic.items():
+            # Add the outer key as a column if needed
+            sub_dict['Building ID'] = key  # Optionally include the outer key as a column
+            writer.writerow(sub_dict)
+def expshp_to_dic(path,keystokeep,keys):
+    #EXPLICAR
+    expdic = shp_to_dict(path)
+    # Iteramos sobre el diccionario principal
+    for dic in expdic.values():
+        # Filtramos el diccionario para conservar solo las claves indicadas
+        for key in list(dic.keys()):
+            if key not in keystokeep:
+                del dic[key]  # Eliminamos las claves que no están en claves_a_conservar
+    for i in range(len(keystokeep)):
+        change_keys_dic(expdic, keystokeep[i], keys[i])
+    return expdic
+def change_keys_dic(dic,oldkey,newkey):
+    for subdic in dic.values():
+            subdic[newkey] = subdic.pop(oldkey)
